4b83bc21101e24e4d8f17dc578de3643669ce104
JulioCSimoesP <juliocsimoesp@gmail.com>
2022-02-10 10:07:51 -0300

    Commit inicial
    Commit inicial do projeto. Adiciona classe de teste de string e classe
    de teste de variável.

---
+++ src/TesteVariavel.java
@@ -0,0 +1,28 @@
+
+public class TesteVariavel {
+
+	public static void main(String[] args) {
+
+		int idade;
+		
+		idade = 37;	
+		System.out.println(idade);
+		
+		idade = 30 + 10;
+		System.out.println(idade);
+		
+		idade = 50 - 12;
+		System.out.println(idade);
+		
+		idade = 5 * 7;
+		System.out.println(idade);
+		
+		idade = 70 / 3;
+		System.out.println(idade);
+		
+		idade = 56 + 62 - 18 * 9 / 2;
+		System.out.println("A idade é " + idade + ". Uau, quantos anos!");
+		
+	}
+
+}
